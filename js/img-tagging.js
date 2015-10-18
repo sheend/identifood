069cb3bb5d280a6cdab2a9f99907796af7dc8e77
Melissa Khuat <mkhuat@cs.washington.edu>
2015-10-18 07:41:42 -0700

    morning update

--- js/img-tagging.js
+++ js/img-tagging.js
@@ -8,12 +8,11 @@
 		
 		var test = document.getElementById("search");
 		test.onClick = function() {
-			var li=document.createElement("li");
-			li.innerHTML="HELLO";
-			document.getElementById("results").appendChild(li);
+			var p = document.createElement("p");
+			p.innerHTML = "HELLO";
+			document.getElementById("results-append").appendChild(p);
 		};
-		document.getElementById("results").style.display = "";
-		
+	
 		// var search = document.getElementById("search");
 		document.getElementById("search").onclick = function() {
 			HttpResponse<String> response = Unirest.get("http://api.imagga.com/v1/tagging?url=http%3A%2F%2Fplayground.imagga.com%2Fstatic%2Fimg%2Fexample_photo.jpg&version=2")
@@ -30,7 +29,11 @@
 					document.getElementById("results").appendChild(li);
 			}
 		};
+	
+	
 	};	
+	
+	
 	
 })();	
 
